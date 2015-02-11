4b2a5c4a8242aef485240248987b449b74c0f160
Nathan Roberson <nathaniroberson@gmail.com>
2015-02-11 14:56:27 -0500

    Adding visibility js

--- graph_menu.js
+++ graph_menu.js
@@ -3,14 +3,15 @@ $(function(){ // on dom ready
 // photos from flickr with creative commons license
   
 var cy = cytoscape({
-  container: document.getElementById('cy'),
+  container: document.getElementById('graph-menu'),
   
   style: cytoscape.stylesheet()
     .selector('node')
       .css({
-        'height': 80,
-        'width': 80,
+        'height': 50,
+        'width': 50,
         'background-fit': 'cover',
+        'content': 'data(id)',
         'border-color': '#000',
         'border-width': 3,
         'border-opacity': 0.5
@@ -30,117 +31,82 @@ var cy = cytoscape({
         'line-color': '#ffaaaa',
         'target-arrow-color': '#ffaaaa'
       })
-    .selector('#bird')
+    .selector('#projects')
       .css({
         'background-image': 'https://farm8.staticflickr.com/7272/7633179468_3e19e45a0c_b.jpg'
       })
-    .selector('#cat')
+    .selector('#about')
       .css({
-        'background-image': 'https://farm2.staticflickr.com/1261/1413379559_412a540d29_b.jpg'
+        'background-image': '/images/drake.jpg'
       })
-    .selector('#ladybug')
+    .selector('#blog')
       .css({
         'background-image': 'https://farm4.staticflickr.com/3063/2751740612_af11fb090b_b.jpg'
       })
-  .selector('#aphid')
+    .selector('#setation')
       .css({
-        'background-image': 'https://farm9.staticflickr.com/8316/8003798443_32d01257c8_b.jpg'
+        'visibility' : 'hidden'
       })
-  .selector('#rose')
+    .selector('#reactto')
       .css({
-        'background-image': 'https://farm6.staticflickr.com/5109/5817854163_eaccd688f5_b.jpg'
-      })
-  .selector('#grasshopper')
-      .css({
-        'background-image': 'https://farm7.staticflickr.com/6098/6224655456_f4c3c98589_b.jpg'
-      })
-  .selector('#plant')
-      .css({
-        'background-image': 'https://farm1.staticflickr.com/231/524893064_f49a4d1d10_z.jpg'
-      })
-  .selector('#wheat')
-      .css({
-        'background-image': 'https://farm3.staticflickr.com/2660/3715569167_7e978e8319_b.jpg'
+        'visibility' : 'hidden'
       }),
   
   elements: {
     nodes: [
-      { data: { id: 'cat' } },
-      { data: { id: 'bird' } },
-      { data: { id: 'ladybug' } },
-      { data: { id: 'aphid' } },
-      { data: { id: 'rose' } },
-      { data: { id: 'grasshopper' } },
-      { data: { id: 'plant' } },
-      { data: { id: 'wheat' } }
+      { data: { id: 'about'} },
+      { data: { id: 'projects'} },
+      { data: { id: 'blog' } },
+      { data: { id: 'resume'}},
+      { data: { id: 'setation', href: 'http://www.vbi.vt.edu'}},
+      { data: { id: 'reactto'}},
     ],
     edges: [
-      { data: { source: 'cat', target: 'bird' } },
-      { data: { source: 'bird', target: 'ladybug' } },
-      { data: { source: 'bird', target: 'grasshopper' } },
-      { data: { source: 'grasshopper', target: 'plant' } },
-      { data: { source: 'grasshopper', target: 'wheat' } },
-      { data: { source: 'ladybug', target: 'aphid' } },
-      { data: { source: 'aphid', target: 'rose' } }
+      { data: {source: 'about', target: 'projects' } },
+      { data: {source: 'about', target: 'blog' } },
+      { data: {source: 'projects', target: 'setation' } },
+      { data: {source: 'projects', target: 'reactto' } },
+      { data: {source: 'about', target: 'resume' } },
+
     ]
   },
   
   layout: {
     name: 'breadthfirst',
     directed: true,
-    padding: 10
-  }
+    padding: 10,
+    animate:true,
+    animationDuration: 500,
+    circle: false
+  },
+
 }); // cy init
-  
+
+cy.userZoomingEnabled( false );
+cy.userPanningEnabled( false );
+
+
+
 cy.on('tap', 'node', function(){
-  var nodes = this;
-  var tapped = nodes;
-  var food = [];
-  
-  nodes.addClass('eater');
-  
-  for(;;){
-    var connectedEdges = nodes.connectedEdges(function(){
-      return !this.target().anySame( nodes );
-    });
-    
-    var connectedNodes = connectedEdges.targets();
-    
-    Array.prototype.push.apply( food, connectedNodes );
-    
-    nodes = connectedNodes;
-    
-    if( nodes.empty() ){ break; }
+
+  var secondLevelEdges = cy.elements('edge[source!="about"]');
+  var secondLevelNodes = secondLevelEdges.targets();
+    if (secondLevelNodes != null){
+    secondLevelNodes.css('visibility', 'hidden');
   }
-        
-  var delay = 0;
-  var duration = 500;
-  for( var i = food.length - 1; i >= 0; i-- ){ (function(){
-    var thisFood = food[i];
-    var eater = thisFood.connectedEdges(function(){
-      return this.target().same(thisFood);
-    }).source();
-            
-    thisFood.delay( delay, function(){
-      eater.addClass('eating');
-    } ).animate({
-      position: eater.position(),
-      css: {
-        'width': 10,
-        'height': 10,
-        'border-width': 0,
-        'opacity': 0
-      }
-    }, {
-      duration: duration,
-      complete: function(){
-        thisFood.remove();
-      }
-    });
-    
-    delay += duration;
-  })(); } // for
-  
+
+  showChildren(this);
+
 }); // on tap
 
 }); // on dom ready
+
+function showChildren(node)
+{
+  var connectedEdges = node.connectedEdges();
+  var connectedNodes = connectedEdges.connectedNodes();
+  if (connectedNodes != null){
+    connectedNodes.css('visibility', 'visible');
+  }
+
+}
